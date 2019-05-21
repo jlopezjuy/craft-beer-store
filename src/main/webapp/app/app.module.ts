a76f1ff75895a62fb754a8c08802cd1455ef35b2
Juan Manuel Lopez <jlopezjuy@gmail.com>
2019-05-21 08:18:00 -0300

    revert mask

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -30,7 +30,7 @@ import { CraftBeerStoreDashboardModule } from 'app/dashboard/dashboard.module';
 import { NgxUiLoaderConfig, NgxUiLoaderModule, PB_DIRECTION, POSITION, SPINNER } from 'ngx-ui-loader';
 import { IConfig, NgxMaskModule } from 'ngx-mask';
 
-export const options: Partial<IConfig> | (() => Partial<IConfig>);
+// export let options: Partial<IConfig> | (() => Partial<IConfig>);
 
 const ngxUiLoaderConfig: NgxUiLoaderConfig = {
     bgsColor: '#OOACC1',
@@ -75,8 +75,8 @@ const ngxUiLoaderConfig: NgxUiLoaderConfig = {
         // jhipster-needle-angular-add-module JHipster will add new module here
         CraftBeerStoreEntityModule,
         CraftBeerStoreAppRoutingModule,
-        NgxUiLoaderModule.forRoot(ngxUiLoaderConfig),
-        NgxMaskModule.forRoot(options)
+        NgxUiLoaderModule.forRoot(ngxUiLoaderConfig)
+        // NgxMaskModule.forRoot(options)
     ],
     declarations: [JhiMainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, ActiveMenuDirective, FooterComponent],
     // entryComponents: [CraftBeerStoreDashboardModule],
