ca26a3b08972d5419e9dab5b64112f02ecb191b3
Juan Manuel Lopez <jlopezjuy@gmail.com>
2019-07-02 17:45:45 -0300

    fix

--- src/main/webapp/app/app.routing.ts
+++ src/main/webapp/app/app.routing.ts
@@ -3,7 +3,7 @@ import { ModuleWithProviders } from '@angular/core';
 import { AuthGuard } from './core';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'admin', pathMatch: 'full' },
+  { path: '', redirectTo: 'admin/dashboard/index', pathMatch: 'full' },
   { path: 'admin', canActivate: [AuthGuard], loadChildren: './admin/admin.module#AdminModule' },
   { path: 'authentication', loadChildren: './authentication/authentication.module#AuthenticationModule' }
 ];
